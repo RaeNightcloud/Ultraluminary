cbc89ffaeec54a50df38ce9e3fb7875f3083500b
Nuryn <nuryn.insyirah02@rafflesgirlssch.edu.sg>
2021-05-09 20:27:57 +0800

    Quick Update
    I put it in Python

--- Intro.py
+++ Intro.py
@@ -1,5 +1,5 @@
-# Ultraluminary 
+print(''' 
 My first repository
 Hi I'm Nuryn and I'm learning Python and GitHub. 
 I am currently taking part in the annual SCDF innovation challenge 2021.
-P.S. I love Vanilla Bean Frappucino but it's way too expensive. 
+P.S. I love Vanilla Bean Frappucino but it's way too expensive. ''')
